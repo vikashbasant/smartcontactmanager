9cfdfe2a428d2c86c445d8f9b817eabd5dcf877e
Vikash Basant <basantvikash360@gmail.com>
2022-05-03 15:58:00 +0530

    Here Creating Responsive Sidebar for Smart Contact Manager

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,15 @@
-console.log("this is script file")
+console.log("this is script file");
+
+const toggleSidebar = () => {
+    if ($(".sidebar").is(":visible")) {
+        // ture
+        // band karna hai
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+    } else {
+        // false
+        // show karna hai
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+    }
+};
